41a47da66afe506036a6ff2a1f75ed1357b52879
Tomer C <17745920+Tomer20@users.noreply.github.com>
2025-07-13 01:29:19 +0300

    Update 03_bastion.tf

--- 03_bastion.tf
+++ 03_bastion.tf
@@ -11,12 +11,11 @@ resource "oci_core_route_table" "public_rt" {
   compartment_id = data.oci_identity_compartment.target.id
   vcn_id         = oci_core_vcn.main_vcn.id
   display_name   = "public-rt"
-
-  route_rules {
+route_rules {
     destination       = "0.0.0.0/0"
     destination_type  = "CIDR_BLOCK"
     network_entity_id = oci_core_internet_gateway.igw[0].id
-  }
+     }
 }
 
 resource "oci_core_security_list" "public_sg" {
